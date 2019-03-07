b7044a52b62caea823cf77b29e768d2f97460589
TheDarven <lucas.minimoys@hotmail.fr>
2019-03-07 18:31:54 +0100

    Index sans piechart

--- index.php
+++ index.php
@@ -54,36 +54,7 @@ include('modele/header.php'); ?>
 					</div>
 				</div>
 			</div>
-			<div id="piechart"></div>
-			<div id="chartContainer" style="height: 300px; width: 100%;">
 		</div>
 	</body>
 	<?php include('modele/footer.php'); ?>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script type="text/javascript">
-		// Load google charts
-		google.charts.load('current', {'packages':['corechart']});
-		google.charts.setOnLoadCallback(drawChart);
-
-		// Draw the chart and set the chart values
-		function drawChart() {
-		  var data = google.visualization.arrayToDataTable([
-		  ['Task', 'Hours per Day'],
-		  ['Work', 8],
-		  ['Friends', 2],
-		  ['Eat', 2],
-		  ['TV', 2],
-		  ['Gym', 2],
-		  ['Sleep', 8]
-		]);
-
-		  // Optional; add a title and set the width and height of the chart
-		  var options = {'title':'My Average Day', 'legend':'none', 'width':550, 'height':400};
-
-		  // Display the chart inside the <div> element with id="piechart"
-		  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-		  chart.draw(data, options);
-		}
-	</script>
-
 </html>
